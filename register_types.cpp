53c50df47afa5595efb00ca5cbfd0ac0a0757bc9
AndreaCatania <info@andreacatania.com>
2022-05-20 07:28:26 +0200

    Upload Network Synchronized for Godot 4.0

---
+++ register_types.cpp
@@ -0,0 +1,60 @@
+/*************************************************************************/
+/*  register_types.cpp                                                   */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+/**
+	@author AndreaCatania
+*/
+
+#include "register_types.h"
+
+#include "core/config/project_settings.h"
+#include "data_buffer.h"
+#include "interpolator.h"
+#include "networked_controller.h"
+#include "scene_diff.h"
+#include "scene_synchronizer.h"
+
+void initialize_network_synchronizer_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SERVERS) {
+		return;
+	}
+
+	GDREGISTER_CLASS(DataBuffer);
+	GDREGISTER_CLASS(SceneDiff);
+	GDREGISTER_CLASS(Interpolator);
+	GDREGISTER_CLASS(NetworkedController);
+	GDREGISTER_CLASS(SceneSynchronizer);
+
+	GLOBAL_DEF("NetworkSynchronizer/debug_server_speedup", false);
+	GLOBAL_DEF("NetworkSynchronizer/debug_doll_speedup", false);
+}
+
+void uninitialize_network_synchronizer_module(ModuleInitializationLevel p_level) {
+}
